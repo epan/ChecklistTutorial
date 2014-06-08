3cfcf2935a0dab018af7fb2a37cacb6d9e0329f0
epan <ericpan@gmail.com>
2014-06-07 22:40:04 -0700

    first commit

---
+++ ChecklistTutorial/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ChecklistTutorial
+//
+//  Created by Eric Pan on 6/7/14.
+//  Copyright (c) 2014 Eric Pan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        // Custom initialization
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
